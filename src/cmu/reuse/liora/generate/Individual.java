419185312d01fb9d0c465eafa59ef588824120ec
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-08 14:31:38 -0400

    adding javadoc comments

--- src/cmu/reuse/liora/generate/Individual.java
+++ src/cmu/reuse/liora/generate/Individual.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author liorafriedberg
+ * represents a person with specific values for the datatypes, corresponds to a row in the database
+ */
 public class Individual {
 	
 	private Map<Column, String> values;
@@ -13,10 +17,17 @@ public class Individual {
 		values = new HashMap<>();
 	}
 	
+	/**
+	 * @param c		column to set
+	 * @param v		value to set
+	 */
 	public void setValue(Column c, String v) {
 		values.put(c, v);
 	}
 	
+	/**
+	 * @return		columns to values for the columns
+	 */
 	public Map<Column, String> getValues() {
 		return values;
 	}
