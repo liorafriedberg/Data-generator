fa463cf5b720815f2053273b9448ebd36914e7b9
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-08 14:35:21 -0400

    adding javadoc comments

--- src/cmu/reuse/liora/generate/Column.java
+++ src/cmu/reuse/liora/generate/Column.java
@@ -4,23 +4,37 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Column {
-	Source source; 
-	String datatype; //column name
+/**
+ * @author liorafriedberg
+ * represents a column in the database, a datatype
+ */
+public class Column { 
+	/**
+	 * column name
+	 */
+	String datatype;
 	String label;
-	boolean percentages; //can use
-	List<Column> dependencies; //can use
-	String[] values; //can use
-	File file; //which file is in
 	
 	public Column() {
-		dependencies = new ArrayList<>();
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 * check if same column name for equality
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == null) return false;
 		Column input = (Column) obj;
-		return (input.datatype.equals(datatype) && input.file.equals(file));
+		return (input.datatype.equals(datatype));
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 * use column name hashcode
+	 */
+	@Override
+	public int hashCode() {
+		return datatype.hashCode(); 
 	}
  }
