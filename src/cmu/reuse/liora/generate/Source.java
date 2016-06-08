70474a773ea67550f5bcb55c805db1f5cd1424b8
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-08 14:31:52 -0400

    adding javadoc comments

--- src/cmu/reuse/liora/generate/Source.java
+++ src/cmu/reuse/liora/generate/Source.java
@@ -1,13 +1,16 @@
 package cmu.reuse.liora.generate;
 
+/**
+ * @author liorafriedberg
+ * options for determining the synthetic data value
+ */
 public enum Source {	
-	//options for determining the synthetic data value
 	PROBS, //prob distribution
 	DEP_PROBS, //depends on prev value, prob distribution
 	DEP_STATIC, //depends on prev value, pre-determined	
 	RANDOM, //random generation
 		RAND_UUID, //uuid
-		RAND_LINE, //random line (column) from file
+		RAND_LINE, //value from column and random line in file
 		RAND_NUMBER; //random number between 0 and 1 (can modify based on needs)
 	
 }
