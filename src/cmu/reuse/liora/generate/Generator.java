e693a7f8f3399d0ea307dcbb72ce835022f1081b
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-15 11:41:40 -0400

    db multivalue integration work

--- src/cmu/reuse/liora/generate/Generator.java
+++ src/cmu/reuse/liora/generate/Generator.java
@@ -5,7 +5,6 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -64,6 +63,7 @@ public class Generator {
 		for (int i = 0; i < numRows; i++) {
 			Individual person = new Individual();
 			Column c = new Column("id");
+			c.source = Source.SEQUENTIAL_LINE; //fine
 			person.setValue(c, i + 1 + "");
 			people.add(person);
 		}
@@ -72,29 +72,36 @@ public class Generator {
 			Source source = menu.getSource(column); //ask user for how want to do it - in order
 			Simulator sim;
 			if (source.equals(Source.PROBS)) {
+				column.source = Source.PROBS;
 				sim = new ProbSim();
 				sim.simulate(menu, column, people, allColumns);
 			} 
 			else if (source.equals(Source.DEP_PROBS_FILE)) {
+				column.source = Source.DEP_PROBS_FILE;
 				sim = new DepProbFileSim();
 				sim.simulate(menu, column, people, allColumns);
 			}
 			else if (source.equals(Source.DEP_PROBS)) {
+				column.source = Source.DEP_PROBS;
 				sim = new DepProbSim();
 				sim.simulate(menu, column, people, allColumns);
 			}			
 			else if (source.equals(Source.DEP_STATIC)) { 
+				column.source = Source.DEP_STATIC;
 				sim = new DepStaticSim();
 				sim.simulate(menu, column, people, allColumns);
 			} 
 			else if (source.equals(Source.DEP_DATE)) {
+				column.source = Source.DEP_DATE;
 				sim = new DepDateSim();
 				sim.simulate(menu, column, people, allColumns);
 			}
 			else if (source.equals(Source.RANDOM)) {
+				column.source = Source.RANDOM;
 				sim = new RandomSim();
 				sim.simulate(menu, column, people, allColumns);
 			} else if (source.equals(Source.MULTI_VALUE)) {
+				column.source = Source.MULTI_VALUE; //can just keep for this one
 				sim = new MultiValueSim();
 				sim.simulate(menu, column, people, allColumns);
 			}
@@ -124,42 +131,112 @@ public class Generator {
 					for (int j = 0; j < addTables.length; j++) {	
 						String newTable = addTables[j];
 						List<Column> newTableCols = menu.getTableCols(newTable); //disease, prescription
-						String key = menu.getKey(newTable);
+						Column mv = menu.getColumn(newTableCols); //or could go through and find without ui
+						String key = menu.getKey(newTable); //need?
+						String mvType = "";
 						String addCreate = "CREATE TABLE " + newTable + "( ";
+						
 						for (Column col : newTableCols) {
-							String type = menu.getType(col); //automate if can
-							if (type.equals("number")) {
+							
+							String val = rows.get(0).getValues().get(col);
+							String type = "";
+							try {
+								Integer.parseInt(val);
 								type = "BIGINT";
-							} 
-							else {
+							} catch(NumberFormatException e) {
 								type = "VARCHAR";
 							}
-							addCreate = addCreate + col.datatype + " " + type + " NOT NULL, ";
-						}
-						addCreate = addCreate.substring(0, addCreate.length() - 2) + ");";
+							if (!col.source.equals(Source.MULTI_VALUE)) {
+								addCreate = addCreate + col.datatype + " " + type + " NOT NULL, ";
+							}
+							else {
+								mvType = type;
+							}
+						}						
+						
+						addCreate = addCreate + mv.datatype + " " + mvType + ");"; //so matches
+						//order below
 
 				System.out.println("addCreate: " + addCreate);
 				stmt = c.prepareStatement(addCreate);
-				stmt.executeUpdate();
-				
-				String addInsert = "INSERT ";
-				for (Column col : newTableCols) { //these all the cols?
-					//fill in here
-				}
-				stmt = c.prepareStatement(addInsert);
 				stmt.executeUpdate();			
+				System.out.println("Please input the multivalue column to assign");
+				//assume is only one mv ?? ****	if not then do with some kind of list
+				
+				for (Individual person : rows) {
+					Map<Column, String> vals = person.getValues();					
+					String[] parts = vals.get(mv).split(",");
+					for (int k = 0; k < parts.length; k++) { //clean everything up
+						String addInsert = "INSERT INTO " + newTable + " VALUES (";	
+							for (Column colTwo : newTableCols) {
+								if (!colTwo.source.equals(Source.MULTI_VALUE)) {
+									addInsert = addInsert + vals.get(colTwo) + ", ";
+								}
+							}
+							addInsert = addInsert + parts[k] + ");";
+				
+							System.out.println("addInsert: " + addInsert);
+							stmt = c.prepareStatement(addInsert);
+							stmt.executeUpdate();	
 					}
+					 					
+				}
+				System.out.println("Please input the value to join on"); // multiple to join on? **
+				Column joinC = menu.getColumn(newTableCols);
+			
+			//is above order okay?? then just go through and assign prescription if need,
+			//and can "join" (id from insurance_member_id?) ****
+					
+			//OR DIFF FROM ABOVE: -------------
+			//insert into medicalrecords insurance_member_id (joiners) how many? 
+					//select insurance_member_id etc
+					//from insurance
+					//where; 				
+					//(or
+					//SELECT insurance_member_id
+				//	INTO medicalrecords
+				//	FROM insurance;)
+				
+					//if do this order: how handle insert multiple times for later mult ds? 
+					
+					//then can join - so get id? 
+					
+					// select insurance.id
+					// into medicalrecords
+					//	from insurance
+					// left join medicalrecords
+					// on insurance.insurance_member_id=medicalrecords.insurance_member_id
+			
+			//get person id and insert disease - but how duplicate rows? one by one? 
+			
+			//update medicalrecords
+			//set disease=first
+			//where record ? 		-- how assign? 
+			
+			//set disease 2
+			//where record ?
+					
+			//------------
+					//are the cols only joiners and multivalue? **
+				    //what table look like? ****
+					
+					//insurance_member_id   disease		prescription
+					//a						cancer		
+					//a						diabetes
+					
+					//how assign prescription? static from disease so already assigned? ****				
+										
 				}
 			}
-			stmt.close();
-			c.commit();
-			c.close();	
+		}
+		stmt.close();
+		c.commit();
+		c.close();	
 		} catch(Exception e) {
 			e.printStackTrace();
 	         System.err.println(e.getClass().getName() + ": " + e.getMessage());
 	         System.exit(0);
-		}
-				
+		}			
 	}	
 	
 	/**
@@ -227,14 +304,17 @@ public class Generator {
 			String create = "CREATE TABLE TOTAL("; //but don't want to recreate now so comment out or drop in cl
 			Map<Column, String> values = rows.get(0).getValues();
 			for (Column col : values.keySet()) {
-				try {
-					Integer.parseInt(values.get(col));
-					create = create + col.datatype + " BIGINT " + "NOT NULL, ";
-				} catch(NumberFormatException e) {
-					create = create + col.datatype + " VARCHAR " + "NOT NULL, ";
+				if (!col.source.equals(Source.MULTI_VALUE)) {
+					try {
+						Integer.parseInt(values.get(col));
+						create = create + col.datatype + " BIGINT " + "NOT NULL, ";
+					} catch(NumberFormatException e) {
+						create = create + col.datatype + " VARCHAR " + "NOT NULL, ";
+					}
 				}
 			}
-			create = create + " PRIMARY KEY (id));";	
+			create = create + " PRIMARY KEY (id));";
+			System.out.println("create: " + create); //check
 			stmt = c.prepareStatement(create);			
 			stmt.executeUpdate();
 			for (Individual i : rows) {
@@ -242,12 +322,15 @@ public class Generator {
 								"VALUES (";
 				
 				Map<Column, String> iValues = i.getValues();
-				for (String s : iValues.values()) {
-					try {
-						Integer.parseInt(s);
-					insert = insert + " " + s + ","; 
-					} catch(NumberFormatException e) {
+				for (Column col : iValues.keySet()) {
+					if (!col.source.equals(Source.MULTI_VALUE)) {
+						String s = iValues.get(col);
+						try {
+							Integer.parseInt(s);
+							insert = insert + " " + s + ","; 
+						} catch(NumberFormatException e) {
 						insert = insert + " '" + s + "', "; //doing this each time because need to add '' if a string and no user input
+						}
 					}
 				} 
 				insert = insert.substring(0, insert.length() - 2); //cut off last , 
