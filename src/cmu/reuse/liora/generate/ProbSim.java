35cdcf2b02b7e6060a571e374ce4dcff32ccd434
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-15 21:07:51 -0400

    adding column check

--- src/cmu/reuse/liora/generate/ProbSim.java
+++ src/cmu/reuse/liora/generate/ProbSim.java
@@ -11,14 +11,16 @@ public class ProbSim implements Simulator {
 		CSVReader reader = new CSVReader(file);
 		System.out.println("Please enter the name of the column with the probabilities.");
 		Column probColumn = menu.getColumn(reader.header);
+		System.out.println("Please enter the name of the column with the possible values.");	
+		Column valColumn = menu.getColumn(reader.header);
 		//value column is column, probColumn is frequency column
 		int format = menu.getDataFormat(); //1 is percentage 2 is freq
 		
 		if (format == 1) {
-			reader.parseProbs(column, probColumn);
+			reader.parseProbs(valColumn, probColumn);
 		}
 		else { //== 2
-			reader.parseFreqs(column, probColumn);
+			reader.parseFreqs(valColumn, probColumn);
 		}				
 		for (Individual person : people) {
 			person.setValue(column, reader.calculate());
