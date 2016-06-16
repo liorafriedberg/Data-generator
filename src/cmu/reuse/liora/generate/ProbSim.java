7b1ca7ce0cd8cd3b762a0236c03b067607a26966
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-16 16:18:11 -0400

    integrates with automation

--- src/cmu/reuse/liora/generate/ProbSim.java
+++ src/cmu/reuse/liora/generate/ProbSim.java
@@ -7,14 +7,14 @@ public class ProbSim implements Simulator {
 
 	@Override
 	public void simulate(Menu menu, Column column, List<Individual> people, List<Column> cols) {
-		File file = menu.getFile();
+		File file = menu.getFile(column);
 		CSVReader reader = new CSVReader(file);
 		System.out.println("Please enter the name of the column with the probabilities.");
-		Column probColumn = menu.getColumn(reader.header);
+		Column probColumn = menu.getProbColumn(reader.header, column);
 		System.out.println("Please enter the name of the column with the possible values.");	
-		Column valColumn = menu.getColumn(reader.header);
+		Column valColumn = menu.getValueColumn(reader.header, column);
 		//value column is column, probColumn is frequency column
-		int format = menu.getDataFormat(); //1 is percentage 2 is freq
+		int format = menu.getDataFormat(column); //1 is percentage 2 is freq
 		
 		if (format == 1) {
 			reader.parseProbs(valColumn, probColumn);
