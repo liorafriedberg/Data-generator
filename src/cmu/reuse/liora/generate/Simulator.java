4993289ea9d5e4146a46355ff23d9d3b9c1c87f3
liorafriedberg <lioraf@sas.upenn.edu>
2016-06-17 13:07:18 -0400

    doc

--- src/cmu/reuse/liora/generate/Simulator.java
+++ src/cmu/reuse/liora/generate/Simulator.java
@@ -2,8 +2,19 @@ package cmu.reuse.liora.generate;
 
 import java.util.List;
 
+/**
+ * generic simulator for generating data from any source
+ * @author liorafriedberg
+ */
 public interface Simulator {
 	
+	/**
+	 * assigns generated values to all people for the current column
+	 * @param menu			Menu for input
+	 * @param column		Current column
+	 * @param people		All people
+	 * @param allColumns	All possible columns
+	 */
 	public void simulate(Menu menu, Column column, List<Individual> people, List<Column> allColumns);
 
 }
